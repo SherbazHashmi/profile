c844ce40d16a28f088f91ea4519ad6dd3b3799bf
Sherbaz <sherbaz.hashmi@gmail.com>
2018-02-23 22:07:11 +1100

    Built iPhone interface

--- Profile/Profile/ViewController.swift
+++ Profile/Profile/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var profilePicture: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        profilePicture.layer.cornerRadius = 2.0
     }
 
     override func didReceiveMemoryWarning() {
