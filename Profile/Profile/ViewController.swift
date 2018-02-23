37cf50b6b37993bdc3a227896a86826e1d749b7c
Sherbaz <sherbaz.hashmi@gmail.com>
2018-02-23 21:25:01 +1100

    Initial Project Files

---
+++ Profile/Profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Profile
+//
+//  Created by Sherbaz Hashmi on 23/2/18.
+//  Copyright © 2018 Sherbaz Hashmi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
